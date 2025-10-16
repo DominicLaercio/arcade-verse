fbc065f2649090ea40b1851e3f4385de5415929f
DominicLaercio <dominiclaercio@outlook.com>
2025-10-16 16:10:55 -0300

    build: Creation of the project with a simple home page to start the development.

---
+++ src/components/Home/Home.tsx
@@ -0,0 +1,20 @@
+import './Home.scss'
+
+
+
+const Home = () => {
+
+    return (
+        <div className="home">
+            <div className="content-area">
+                <div className="content">
+                    <p>Content</p>
+                    <br />
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis, nihil rem quaerat laudantium dolores vitae, ex culpa qui magnam commodi cum porro pariatur modi minus magni iure velit excepturi recusandae.</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
